9ceb61faf3009bfc9a9df7a5afd5d97a6734019e
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-03 09:49:55 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import google.cloud.dialogflow as dialogflow
-from google.cloud.dialogflow_v2beta1.services import conversations_client
+from google.cloud.dialogflow import conversations_client
 from google.protobuf import field_mask_pb2
 
 # Define the Streamlit app
